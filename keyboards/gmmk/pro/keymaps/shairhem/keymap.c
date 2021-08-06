3149694bfcf6bc4e05ac92284bdcab208384a340
Shairhem Fernandez <sfernandez@you-source.com>
2021-08-07 03:48:07 +0800

    Add led timeout

--- keyboards/gmmk/pro/keymaps/shairhem/keymap.c
+++ keyboards/gmmk/pro/keymaps/shairhem/keymap.c
@@ -13,14 +13,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include "timer.h"
 #include QMK_KEYBOARD_H
 
 #define _BASE 0
 
 #define _BASE_COLOR_RGB 0, 15, 40
 #define _CAPS_COLOR_RGB 80, 0, 0
+#define _BASE_COLOR_OFF 0, 0, 0
 
+#define RGB_TIMEOUT 960000 // 15 minutes
 #define ESC_KEY 9
+
+static uint32_t key_timer = 0;
+static bool rgb_suspended = true;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
@@ -35,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -76,6 +82,23 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
 
     return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    key_timer = timer_read32();
+    return true;
+}
+
+void matrix_scan_user(void) { 
+    if (timer_elapsed32(key_timer) > RGB_TIMEOUT) { // 30 seconds
+        rgb_suspended = true;
+        rgb_matrix_set_color_all(_BASE_COLOR_OFF);
+    } else {
+        if (rgb_suspended) {
+            rgb_suspended = false;
+            rgb_matrix_indicators_user();
+        }
+    }
 }
 
 void rgb_matrix_indicators_user(void) {
